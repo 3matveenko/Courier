e2fc043fc8213aa980e77b30396305e4c1a1bbe4
3matveenko <3matveenkokz@gmail.com>
2023-09-14 18:00:29 +0600

    14.09

--- app/src/main/java/com/example/courier/MainActivity.kt
+++ app/src/main/java/com/example/courier/MainActivity.kt
@@ -1,17 +1,31 @@
 package com.example.courier
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
-
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val sharedPreferences = getSharedPreferences("local_storage", MODE_PRIVATE)
-        val editor = sharedPreferences.edit()
-        editor.putString("server_name", "0000")
-        //*************************************
-        editor.apply()
         setContentView(R.layout.activity_main)
+
+        val myTextView = findViewById<TextView>(R.id.mainTextview)
+
+        val sharedPreferences = getSharedPreferences("local_storage", Context.MODE_PRIVATE)
+        val serverName = sharedPreferences.getString("server_name", "0")
+
+        if (serverName == "0") {
+            myTextView.setText("Локальное хранилище пусто")
+        } else {
+            myTextView.setText(serverName)
+        }
+
+        // Редактирование SharedPreferences
+        val editor = sharedPreferences.edit()
+        editor.putString("server_name", "имя сервера")
+        editor.apply()
     }
 }
